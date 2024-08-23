716435a6e1cbe53cdcb495dc1b63d1abf7bd43f4
enze <csudez3625@163.com>
2024-08-22 21:15:40 -0700

    [dev]: change to tf2.

--- attacks/cw_2_atk.py
+++ attacks/cw_2_atk.py
@@ -10,16 +10,11 @@ conf = 0                # How strong the adversarial example should be
 init_const = 1e-3       # The initial constant c to pick as a first guess
 
 
-# Carlini Wagner Attack, L_2, from 2017 S&P
-class CwL2Attack:
-    def __init__(self, sess, model, batch_size=1, confidence = conf, targeted = targeted, learning_rate = lr,
-                 binary_search_steps = binary_search_step, max_iterations = max_iter, abort_early = abort_early, 
-                 initial_const = init_const, boxmin = -0.5, boxmax = 0.5):
-
-
-        image_size, num_channels, num_labels = model.image_size, model.num_channels, model.num_labels
-        
-        self.sess = sess
+# Carlini Wagner Attack, L_2
+class CwL2:
+    def __init__(self, model, batch_size=1, confidence=conf, targeted=targeted, learning_rate=lr, binary_search_steps=binary_search_step, max_iterations=max_iter, abort_early=abort_early, initial_const=init_const, boxmin=-0.5, boxmax=0.5):
+        # image_size, num_channels, num_labels = model.image_size, model.num_channels, model.num_labels
+        image_size, num_channels, num_labels = 28, 1, 10
         self.TARGETED = targeted
         self.LEARNING_RATE = learning_rate
         self.MAX_ITERATIONS = max_iterations
@@ -33,76 +28,37 @@ class CwL2Attack:
 
         self.I_KNOW_WHAT_I_AM_DOING_AND_WANT_TO_OVERRIDE_THE_PRESOFTMAX_CHECK = False
 
-        shape = (batch_size,image_size,image_size,num_channels)
-        
-        # the variable we're going to optimize over
-        modifier = tf.Variable(np.zeros(shape,dtype=np.float32))
-
-        # these are variables to be more efficient in sending data to tf
-        self.timg = tf.Variable(np.zeros(shape), dtype=tf.float32)
-        self.tlab = tf.Variable(np.zeros((batch_size,num_labels)), dtype=tf.float32)
-        self.const = tf.Variable(np.zeros(batch_size), dtype=tf.float32)
-
-        # and here's what we use to assign them
-        self.assign_timg = tf.placeholder(tf.float32, shape)
-        self.assign_tlab = tf.placeholder(tf.float32, (batch_size,num_labels))
-        self.assign_const = tf.placeholder(tf.float32, [batch_size])
-        
-        # the resulting image, tanh'd to keep bounded from boxmin to boxmax
+        self.shape = (batch_size, image_size, image_size, num_channels)
         self.boxmul = (boxmax - boxmin) / 2.
         self.boxplus = (boxmin + boxmax) / 2.
-        self.newimg = tf.tanh(modifier + self.timg) * self.boxmul + self.boxplus
-        
-        # prediction BEFORE-SOFTMAX of the model
-        self.output = model.predict(self.newimg)
-        
-        # distance to the input data
-        self.l2dist = tf.reduce_sum(tf.square(self.newimg-(tf.tanh(self.timg) * self.boxmul + self.boxplus)),[1,2,3])
-        
-        # compute the probability of the label class versus the maximum other
-        real = tf.reduce_sum((self.tlab)*self.output,1)
-        other = tf.reduce_max((1-self.tlab)*self.output - (self.tlab*10000),1)
-
-        if self.TARGETED:
-            # if targetted, optimize for making the other class most likely
-            loss1 = tf.maximum(0.0, other-real+self.CONFIDENCE)
-        else:
-            # if untargeted, optimize for making this class least likely.
-            loss1 = tf.maximum(0.0, real-other+self.CONFIDENCE)
-
-        # sum up the losses
-        self.loss2 = tf.reduce_sum(self.l2dist)
-        self.loss1 = tf.reduce_sum(self.const*loss1)
-        self.loss = self.loss1+self.loss2
-        
-        # Setup the adam optimizer and keep track of variables we're creating
-        start_vars = set(x.name for x in tf.global_variables())
-        optimizer = tf.train.AdamOptimizer(self.LEARNING_RATE)
-        self.train = optimizer.minimize(self.loss, var_list=[modifier])
-        end_vars = tf.global_variables()
-        new_vars = [x for x in end_vars if x.name not in start_vars]
-
-        # these are the variables to initialize when we run
-        self.setup = []
-        self.setup.append(self.timg.assign(self.assign_timg))
-        self.setup.append(self.tlab.assign(self.assign_tlab))
-        self.setup.append(self.const.assign(self.assign_const))
-        
-        self.init = tf.variables_initializer(var_list=[modifier]+new_vars)
+        self.model = model
 
     def attack(self, imgs, targets):
+        """
+        If self.targeted is true, then the targets represents the target labels.
+        If self.targeted is false, then targets are the original class labels.
+        """
         adv_res = []
-        print('go up to',len(imgs))
-        for i in range(0,len(imgs),self.batch_size):
-            print('tick',i)
-            adv_res.extend(self.attack_batch(imgs[i:i+self.batch_size], targets[i:i+self.batch_size]))
+        print('go up to', len(imgs))
+        for i in range(0, len(imgs), self.batch_size):
+            print('tick', i)
+            # print("imgs[i:i + self.batch_size]", imgs[i:i + self.batch_size])
+            # print("targets", targets)
+            adv_res.extend(self.attack_batch(
+                imgs[i:i + self.batch_size], targets))
         return np.array(adv_res)
 
     def attack_batch(self, imgs, labs):
+        """
+        Run the attack on a batch of images and labels.
+        """
+        # print("imgs, labs in attack_batch", imgs, labs) #shape=(1, 28, 28, 1), dtype=float32) [array([0., 0., 0., 0., 0., 0., 1., 0., 0., 0.])]
+        batch_size = self.batch_size
         
         # whether attack success
         def check_success(x, y):
             if not isinstance(x, (float, int, np.int64)):
+                x = x.numpy()
                 x = np.copy(x)
                 if self.TARGETED:
                     x[y] -= self.CONFIDENCE
@@ -114,64 +70,85 @@ class CwL2Attack:
             else:
                 return x != y
 
-        batch_size = self.batch_size
+        # @tf.function
+        def train_step(modifier, timg, tlab, const):
+            with tf.GradientTape() as tape:
+                newimg = tf.tanh(modifier + timg) * self.boxmul + self.boxplus
+                output = self.model(newimg)
+                output = tf.cast(output, dtype=tf.float32)
+                l2dist = tf.reduce_sum(
+                    tf.square(newimg - (tf.tanh(timg) * self.boxmul + self.boxplus)), [1, 2, 3])
+                real = tf.math.reduce_sum((tlab) * output, 1)
+                other = tf.math.reduce_max(
+                    (1 - tlab) * output - (tlab * 10000), 1)
+                if self.TARGETED:
+                    # if targetted, optimize for making the other class most likely
+                    loss1 = tf.maximum(0.0, other - real + self.CONFIDENCE)
+                else:
+                    # if untargeted, optimize for making this class least likely.
+                    loss1 = tf.maximum(0.0, real - other + self.CONFIDENCE)
+
+                loss2 = tf.reduce_sum(l2dist)
+                loss1 = tf.reduce_sum(const * loss1)
+
+                loss = loss1 + loss2
+            optimizer = tf.optimizers.Adam(self.LEARNING_RATE)
+            loss_metric = tf.keras.metrics.Mean(name='train_loss')
+
+            grads = tape.gradient(loss, [modifier])
+            optimizer.apply_gradients(zip(grads, [modifier]))
+            loss_metric.update_state(loss)
+            return loss, l2dist, output, newimg, loss1, loss2
 
         # convert to tanh-space
         imgs = np.arctanh((imgs - self.boxplus) / self.boxmul * 0.999999)
-
-        # set the lower and upper bounds accordingly
+        # print(np.shape(imgs))
         lower_bound = np.zeros(batch_size)
-        CONST = np.ones(batch_size)*self.initial_const
-        upper_bound = np.ones(batch_size)*1e10
+        CONST = np.ones(batch_size) * self.initial_const
+        upper_bound = np.ones(batch_size) * 1e10
 
         # the best l2, score, and image attack
-        o_bestl2 = [1e10]*batch_size
-        o_bestscore = [-1]*batch_size
-        o_bestattack = [np.zeros(imgs[0].shape)]*batch_size
-        
-        for outer_step in range(self.BINARY_SEARCH_STEPS):
-            print(o_bestl2)
-            # completely reset adam's internal state.
-            self.sess.run(self.init)
-            batch = imgs[:batch_size]
-            batchlab = labs[:batch_size]
-    
-            bestl2 = [1e10]*batch_size
-            bestscore = [-1]*batch_size
+        o_bestl2 = [1e10] * batch_size
+        o_bestscore = [-1] * batch_size
+        o_bestattack = [np.zeros(imgs[0].shape)] * batch_size
+        print(np.shape(o_bestattack), "np.shape(o_bestattack)")  # (1, 28, 28, 1)
 
-            # The last iteration (if we run many steps) repeat the search once.
-            if self.repeat == True and outer_step == self.BINARY_SEARCH_STEPS-1:
+        for outer_step in range(self.BINARY_SEARCH_STEPS):
+            batch = tf.Variable(imgs[:batch_size], dtype=tf.float32)
+            batchlab = tf.Variable(labs[:batch_size], dtype=tf.float32)
+            bestl2 = [1e10] * batch_size
+            bestscore = [-1] * batch_size
+            if self.repeat == True and outer_step == self.BINARY_SEARCH_STEPS - 1:
                 CONST = upper_bound
 
-            # set the variables so that we don't have to send them over again
-            self.sess.run(self.setup, {self.assign_timg: batch,
-                                       self.assign_tlab: batchlab,
-                                       self.assign_const: CONST})
-            
+            modifier = tf.Variable(np.zeros((1, 28, 28, 1), dtype=np.float32))
+            const = tf.Variable(CONST, dtype=tf.float32)
             prev = np.inf
             for iteration in range(self.MAX_ITERATIONS):
-                # perform the attack 
-                _, l, l2s, scores, nimg = self.sess.run([self.train, self.loss, 
-                                                         self.l2dist, self.output, 
-                                                         self.newimg])
+                # perform the attack
 
-                if np.all(scores>=-.0001) and np.all(scores <= 1.0001):
-                    if np.allclose(np.sum(scores,axis=1), 1.0, atol=1e-3):
+                l, l2s, scores, nimg, loss1, loss2 = train_step(
+                    modifier, batch, batchlab, const)
+
+                if np.all(scores >= -.0001) and np.all(scores <= 1.0001):
+                    if np.allclose(np.sum(scores, axis=1), 1.0, atol=1e-3):
                         if not self.I_KNOW_WHAT_I_AM_DOING_AND_WANT_TO_OVERRIDE_THE_PRESOFTMAX_CHECK:
                             raise Exception("The output of model.predict should return the pre-softmax layer. It looks like you are returning the probability vector (post-softmax). If you are sure you want to do that, set attack.I_KNOW_WHAT_I_AM_DOING_AND_WANT_TO_OVERRIDE_THE_PRESOFTMAX_CHECK = True")
-                
-                # print out the losses every 10%
-                if iteration%(self.MAX_ITERATIONS//10) == 0:
-                    print(iteration,self.sess.run((self.loss,self.loss1,self.loss2)))
 
+                if iteration % (self.MAX_ITERATIONS // 10) == 0:
+                    print(iteration, l, loss1, loss2)
                 # check if we should abort search if we're getting nowhere.
-                if self.ABORT_EARLY and iteration%(self.MAX_ITERATIONS//10) == 0:
-                    if l > prev*.9999:
+                if self.ABORT_EARLY and iteration % (self.MAX_ITERATIONS // 10) == 0:
+                    if l > prev * .9999:
                         break
                     prev = l
-
                 # adjust the best result found so far
-                for e,(l2,sc,ii) in enumerate(zip(l2s,scores,nimg)):
+                for e, (l2, sc, ii) in enumerate(zip(l2s, scores, nimg)):
+                    # print("batchlab", np.argmax(batchlab[e]))
+                    # print("(sc, np.argmax(batchlab))", sc, np.argmax(sc))
+                    # print("l2 and bestl2[e]", l2, bestl2[e])
+                    # print("compare(sc, tf.argmax(batchlab))",
+                    #       compare(sc, tf.argmax(batchlab[e])))
                     if l2 < bestl2[e] and check_success(sc, np.argmax(batchlab[e])):
                         bestl2[e] = l2
                         bestscore[e] = np.argmax(sc)
@@ -182,20 +159,19 @@ class CwL2Attack:
 
             # adjust the constant as needed
             for e in range(batch_size):
+                print("bestscore[e]", bestscore[e])
                 if check_success(bestscore[e], np.argmax(batchlab[e])) and bestscore[e] != -1:
                     # success, divide const by two
-                    upper_bound[e] = min(upper_bound[e],CONST[e])
+                    upper_bound[e] = min(upper_bound[e], CONST[e])
                     if upper_bound[e] < 1e9:
-                        CONST[e] = (lower_bound[e] + upper_bound[e])/2
+                        CONST[e] = (lower_bound[e] + upper_bound[e]) / 2
                 else:
                     # failure, either multiply by 10 if no solution found yet
                     #          or do binary search with the known upper bound
-                    lower_bound[e] = max(lower_bound[e],CONST[e])
+                    lower_bound[e] = max(lower_bound[e], CONST[e])
                     if upper_bound[e] < 1e9:
-                        CONST[e] = (lower_bound[e] + upper_bound[e])/2
+                        CONST[e] = (lower_bound[e] + upper_bound[e]) / 2
                     else:
                         CONST[e] *= 10
-
-        # return the best solution found
         o_bestl2 = np.array(o_bestl2)
         return o_bestattack
